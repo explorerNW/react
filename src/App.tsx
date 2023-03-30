f84cb14933deb8ade1b0ba3d57bbbfa5517e76ab
explorerNW <superexplorernw@163.com>
2023-03-30 20:29:11 +0800

    feat(feat-2): add bootstrap reactstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,38 @@
+import { lazy } from "react";
+import React, { Suspense } from "react";
+
 import logo from "./logo.svg";
 import "./App.css";
+import { ComponentA } from "./components";
+
+const ComponentB = lazy(() => import("./components/component-b"));
+class Temp extends React.Component {
+  clickHandler(e: any) {
+    console.log(e);
+    import("./components/component-b").then((component) => {
+      console.log(component);
+    });
+  }
+
+  render() {
+    return (
+      <>
+        <span onClick={this.clickHandler}>Temp</span>
+      </>
+    );
+  }
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ComponentA />
+        <Temp></Temp>
+        <Suspense fallback={<div>loading...</div>}>
+          <ComponentB></ComponentB>
+        </Suspense>
       </header>
     </div>
   );
